d3f3fa9b2b0df745e34f6ef842419cdfceba7849
hatiolab <shortstop@hatiolab.com>
2013-11-28 21:22:10 +0900

    Fix

--- vendor/bundles/bar/app/models/trade.rb
+++ vendor/bundles/bar/app/models/trade.rb
@@ -1,7 +1,7 @@
 class Trade < ActiveRecord::Base
 
 	stampable
-	meaningful_id [:domain_id, :name]
+	meaningful_id [:name]
 	belongs_to :domain
 	attr_accessible :name,:description,:attr_nm,:tr_fg,:reg_nb,:ppl_nb,:ceo_nm,:business,:jongmok,:zip,:div_addr1,:addr2,:ddd,:tel,:fax,:tr_nmk,:attr_nmk,:ceo_nmk,:use_yn
 
